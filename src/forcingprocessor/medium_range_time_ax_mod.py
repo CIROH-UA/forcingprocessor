58125b7ad9a71391fea548690a5dc7a85e0086b7
Quinn Lee <qylee@ua.edu>
2025-12-03 14:38:25 -0600

    fix medium range lag shifter

--- src/forcingprocessor/medium_range_time_ax_mod.py
+++ src/forcingprocessor/medium_range_time_ax_mod.py
@@ -1,5 +1,5 @@
 # Author: Jordan Laser, Lynker
-# 
+#
 # This script reads in a NRDS forcing file and shifts the time coordinate for medium range ensemble members
 # https://github.com/CIROH-UA/ngen-datastream/issues/202
 import xarray as xr
@@ -15,12 +15,12 @@ def cut_forcing_data_for_ensemble(ds:xr.Dataset, ens_member:int, time_shift_hour
     Shift the time axis of the dataset based on the ensemble member.
 
     # https://onlinelibrary.wiley.com/doi/epdf/10.1111/1752-1688.13184
-    
+
     Parameters:
     ds (xarray.Dataset): Input dataset with Time coordinate.
     ens_member (int): Ensemble member number (2-7).
     time_shift_hours (int): Number of hours to shift the time coordinate.
-    
+
     Returns:
     np.ndarray: Shifted time axis.
     """
@@ -66,9 +66,9 @@ if __name__ == "__main__":
     ens_member = int(args.ensemble_member)
     if ens_member > 1 and ens_member < 8:
         ds_mod = cut_forcing_data_for_ensemble(ds_in, ens_member, time_shift_hours)
-    else: 
+    else:
         raise ValueError("Ensemble member must be between 2 and 7")
-    
+
     # Choose filename
     pattern = r'^ngen\.t\d{2}z\.medium_range\.forcing\.f001_f240\.VPU_\d+\.nc$'
     input_file = Path(args.input_file_ens0).name
@@ -76,15 +76,29 @@ if __name__ == "__main__":
         out_filename = input_file.replace('f001_f240', 'f001_f204')
     else:
         out_filename = "forcings_ens_" + str(ens_member) + ".nc"
-    out_path = Path(args.output_dir) / out_filename 
+    out_path = Path(args.output_dir) / out_filename
 
     # Create data array for make_forcing_netcdf
     nvar = len(ds_in.variables) - 2  # Exclude the time and catchment ids variable
-    ncat= ds_in["UGRD_10maboveground"].shape[0]
+
+    if "UGRD_10maboveground" in ds_in.variables:
+        file_type = "forcings"
+    else:
+        file_type = "channel_routing"
+
+    if file_type == "forcings":
+        ncat= ds_in["UGRD_10maboveground"].shape[0]
+    else:
+        ncat = ds_in['q_lateral'].shape[0]
     data_array = np.ones((ncat,204,nvar),dtype='float64')
     vars = list(ds_mod.keys())
-    vars.remove('Time')
-    vars.remove('ids')
+
+    if file_type == "forcings":
+        vars.remove('Time')
+        vars.remove('ids')
+    else:
+        vars.remove('time')
+        vars.remove('feature_id')
     for j, jvar in enumerate(vars):
         data_array[:,:,j] = ds_mod[jvar].values
 
