b9c8e188f9a46d818e11746a41fefd6ec0c5727e
Quinn Lee <qylee@ua.edu>
2026-01-29 13:17:51 -0600

    channel routing data extraction (#46)
    * extract q_lat from nwm files
    
    * update gitignore
    
    * can specify subset of US to process
    
    * write to netcdf implemented
    
    * got stats working
    
    * fixed csv generation
    
    * fixed tarball writing
    
    * added channel routing pytests
    
    * updated readme
    
    * fix example config typo
    
    * fix typo in pytest, specify nc engine
    
    * revert reordering in pytest file
    
    * fix s3 netcdf write error
    
    * allow reading map file from s3
    
    * error handling for missing IDs in mapping dict
    
    * added dask for chunking and also vectorized computations
    
    * now configured to require a gpkg to subset the mapping file, maintains parity with other datastreams
    
    * fix test
    
    * fix test
    
    * fix medium range lag shifter
    
    * Revert "fix medium range lag shifter"
    
    This reverts commit 58125b7ad9a71391fea548690a5dc7a85e0086b7.
    
    * fixed dataframe writing format
    
    * fixed parquet writing
    
    * fix csv format
    
    * fix missing args
    
    * fixed qlateral calculation

---
+++ src/forcingprocessor/channel_routing_tools.py
@@ -0,0 +1,179 @@
+"""Tools to extract and write q_lateral values into a format ingestible by
+t-route. Translates between NWM and NGEN IDs!"""
+
+import itertools
+import os
+from io import BytesIO
+import time
+from datetime import datetime
+from pathlib import Path
+import gcsfs
+import requests
+import xarray as xr
+import numpy as np
+import pandas as pd
+import boto3
+import traceback
+import tempfile
+from forcingprocessor.utils import convert_url2key, report_usage, make_forcing_netcdf
+
+B2MB = 1048576
+
+def channelrouting_nwm2ngen(nwm_files: list,
+                            mapping_arg: dict,
+                            fs_type_arg: str,
+                            fs_arg = None,
+                            ii_verbose_arg: bool = False
+                            ):
+    """
+    Retrieve catchment level data from national water model files
+
+    Inputs:
+    nwm_files (list): list of filenames (urls for remote, local paths otherwise),
+    fs_arg (filesystem): an optional file system for cloud storage reads
+    mapping_arg (dict): dictionary of NWM to NGEN ID maps
+    fs_type_arg (str): type of file system
+    ii_verbose_arg (bool): verbosity
+
+    Outputs: [data_list, t_list, nwm_file_sizes_MB]
+    data_list (list): list of ngen forcings ordered in time.
+    t_list (list): list of model output times
+    nwm_file_sizes_MB (list): list of file sizes of input CHRTOUT data
+    """
+    topen = 0
+    txrds = 0
+    tfill = 0
+    tdata = 0
+    t_list = []
+    nfiles = len(nwm_files)
+    nwm_cats = list(itertools.chain.from_iterable(list(mapping_arg.values())))
+    if fs_type_arg == 'google' :
+        fs_arg = gcsfs.GCSFileSystem()
+    pid = os.getpid()
+    if ii_verbose_arg:
+        print(f'Process #{pid} extracting data from {nfiles} files',end=None,flush=True)
+    data_list = []
+    nwm_file_sizes_MB = []
+    for j, nwm_file in enumerate(nwm_files):
+        t0 = time.perf_counter()
+        if fs_arg:
+            if nwm_file.find('https://') >= 0:
+                _, bucket_key = convert_url2key(nwm_file,fs_type_arg)
+            else:
+                bucket_key = nwm_file
+            file_obj   = fs_arg.open(bucket_key, mode='rb')
+            nwm_file_sizes_MB.append(file_obj.details['size'])
+        elif 'https://' in nwm_file:
+            response = requests.get(nwm_file, timeout=10)
+
+            if response.status_code == 200:
+                file_obj = BytesIO(response.content)
+            else:
+                raise RuntimeError(f"{nwm_file} does not exist")
+            nwm_file_sizes_MB.append(len(response.content) / B2MB)
+        else:
+            file_obj = nwm_file
+            nwm_file_sizes_MB.append(os.path.getsize(nwm_file / B2MB))
+
+        topen += time.perf_counter() - t0
+        t0 = time.perf_counter()
+        with xr.open_dataset(file_obj,chunks={}) as nwm_data:
+            txrds += time.perf_counter() - t0
+            t0 = time.perf_counter()
+            data_allnwm = {}
+            try:
+                subset = nwm_data.sel(feature_id=nwm_cats)
+                valid_nwm_cats = nwm_cats
+            except KeyError:
+                print(f"Some NWM IDs from the mapping are not present in {nwm_file}. Only " +
+                      "processing available IDs.",
+                      flush=True)
+                feature_ids_in_file = set(nwm_data['feature_id'].values)
+                valid_nwm_cats = feature_ids_in_file.intersection(nwm_cats)
+                subset = nwm_data.sel(feature_id=list(valid_nwm_cats))
+            if "retrospective" in nwm_file:
+                data_allnwm = dict(zip(subset['feature_id'].values,subset['q_lateral'].values))
+                t = datetime.strftime(datetime.strptime(
+                    nwm_file.split('/')[-1].split('.')[0],'%Y%m%d%H%M'),'%Y-%m-%d %H:%M:%S')
+            else:
+                # q_lateral is calculated by adding these two together
+                subset['q_lateral'] = subset['qSfcLatRunoff'] + subset['qBucket']
+                data_allnwm = dict(zip(subset['feature_id'].values,subset['q_lateral'].values))
+                time_splt = subset.attrs["model_output_valid_time"].split("_")
+                t = time_splt[0] + " " + time_splt[1]
+            t_list.append(t)
+        del nwm_data, subset
+        tfill += time.perf_counter() - t0
+
+        t0 = time.perf_counter()
+        data_allngen = {}
+        valid_nwm_set = set(valid_nwm_cats)
+        for ngen_nex, nwm_ids in mapping_arg.items():
+            data_allngen[ngen_nex] = sum(
+                data_allnwm[nwm_id] for nwm_id in nwm_ids if nwm_id in valid_nwm_set
+            )
+        data_array = np.array(list(data_allngen.items()))
+
+        data_list.append(data_array)
+        tdata += time.perf_counter() - t0
+        ttotal = topen + txrds + tfill + tdata
+        if ii_verbose_arg:
+            print(f'\nAverage time for:\nfs open file: {topen/(j+1):.2f} s\n', end=None,flush=True)
+            print(f'xarray open dataset: {txrds/(j+1):.2f} s\nfill array: {tfill/(j+1):.2f} s\n',
+                  end=None,flush=True)
+            print(f'calculate catchment values: {tdata/(j+1):.2f} s\ntotal {ttotal/(j+1):.2f} s\n',
+                  end=None,flush=True)
+            print(f'percent complete {100*(j+1)/nfiles:.2f}', end=None,flush=True)
+        report_usage()
+
+    if ii_verbose_arg:
+        print(f'Process #{pid} completed data extraction, returning data to primary process',
+              flush=True)
+    return [data_list, t_list, nwm_file_sizes_MB]
+
+def write_netcdf_chrt(storage_type: str, prefix: Path, data: np.ndarray, times: list, name: str):
+    """
+    Write channel routing data to a NetCDF file.
+
+    Parameters:
+        storage_type (str): s3 or local
+        prefix (Path): filename prefix
+        data (numpy.ndarray): 2D array with dimensions (nexus-id, qlateral).
+        times (list): list representing time axis.
+        name (str): string for the filename
+    Returns:
+        netcdf_cat_file_size (list): file size of output netcdf
+    """
+    if storage_type == 's3':
+        s3_client = boto3.session.Session().client("s3")
+        nc_filename = str(prefix) + "/" + name
+    else:
+        nc_filename = Path(prefix, name)
+
+    time_coord = pd.to_datetime(times)
+    feature_ids = data[0, :, 0]
+    q_lateral = data[:, :, 1].astype(float)
+
+    ds = xr.Dataset(
+    {
+        "q_lateral": (("time", "feature_id"), q_lateral)
+    },
+    coords={
+        "time": time_coord,
+        "feature_id": feature_ids
+        }
+    )
+    if storage_type == 's3':
+        bucket, key = convert_url2key(nc_filename,'s3')
+        with tempfile.NamedTemporaryFile(suffix='.nc') as tmpfile:
+            ds.to_netcdf(tmpfile.name, engine="netcdf4")
+            netcdf_cat_file_size = os.path.getsize(tmpfile.name) / B2MB
+            tmpfile.flush()
+            tmpfile.seek(0)
+            print(f"Uploading netcdf forcings to S3: bucket={bucket}, key={key}")
+            s3_client.upload_file(tmpfile.name, bucket, key)
+    else:
+        ds.to_netcdf(nc_filename, engine="netcdf4")
+        print(f'netcdf has been written to {nc_filename}')
+        netcdf_cat_file_size = os.path.getsize(nc_filename) / B2MB
+    return [netcdf_cat_file_size]
