f91abafbd3a9f9489ce61c564787a58cbb7fbb31
Pratiksha Chaudhari <pratikshac15@gmail.com>
2026-08-13 21:03:02 -0500

    Update remaining GeoPackage references to v2.2

--- tests/test_hf2ds.py
+++ tests/test_hf2ds.py
@@ -10,8 +10,6 @@ out_parq = os.path.join(data_dir, "out.parquet")
 parq_name = "09_weights.parquet"
 parq_path = os.path.join(data_dir, parq_name)
 
-geopackage_name = "palisade.gpkg"
-gpkg_path = os.path.join(data_dir, geopackage_name)
 raster = "https://noaa-nwm-retrospective-3-0-pds.s3.amazonaws.com/CONUS/netcdf/FORCING/2018/201801010000.LDASIN_DOMAIN1"
 
 
@@ -23,13 +21,6 @@ def test_parquet_v21():
     # weights,_ = hf2ds([parq_path],raster,1)
     # assert len(weights) > 0
 
-
-def test_gpkg_v21():
-    os.system(
-        f"curl -o {gpkg_path} -L -O https://ciroh-community-ngen-datastream.s3.amazonaws.com/resources/v2.1_hydrofabric/geopackages/test_data/{geopackage_name}"
-    )
-    weights, _ = hf2ds([gpkg_path], raster, 1)
-    assert len(weights) > 0
 
 
 def test_gpkg_v22():
@@ -52,13 +43,6 @@ def test_multiple_parquet_v21():
     # assert len(weights) > 0
 
 
-def test_multiple_gpkg_v21():
-    os.system(
-        f"curl -o {gpkg_path} -L -O https://ciroh-community-ngen-datastream.s3.amazonaws.com/resources/v2.1_hydrofabric/geopackages/test_data/{geopackage_name}"
-    )
-    weights, _ = hf2ds([gpkg_path, gpkg_path], raster, 1)
-    assert len(weights) > 0
-
 
 def test_multiple_gpkg_v22():
     weights, _ = hf2ds(
@@ -80,13 +64,6 @@ def test_multiple_multiprocess_parquet_v21():
     # weights,_ = multiprocess_hf2ds([parq_path,parq_path],raster,2)
     # assert len(weights) > 0
 
-
-def test_multiple_multiprocess_gpkg_v21():
-    os.system(
-        f"curl -o {gpkg_path} -L -O https://ciroh-community-ngen-datastream.s3.amazonaws.com/resources/v2.1_hydrofabric/geopackages/test_data/{geopackage_name}"
-    )
-    weights, _ = multiprocess_hf2ds([gpkg_path, gpkg_path], raster, 2)
-    assert len(weights) > 0
 
 
 def test_multiple_multiprocess_gpkg_v22():
